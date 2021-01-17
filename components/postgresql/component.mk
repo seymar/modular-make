3fea488096e12451ba2d2ce1d416b58d0cc5dc89
Stijn Martens <seymar@users.noreply.github.com>
2021-01-17 02:56:32 +0100

    unrelated

--- components/postgresql/component.mk
+++ components/postgresql/component.mk
@@ -11,9 +11,6 @@ DOWNLOADS := $(GZURL)
 # - Can add include paths to the parent PUBLIC_INCLUDES
 # - Can add a static library (.a) to the top-level linker
 # - Static libraries can be build by the main Makefile or with a custom target
-# - Nothing should happen if there is no dependency on this component, to make
-#   sure the build system is as efficient as possible
-# - A component makefile should not be included if there is no dependency on it, possible?
 postgresql-build: $(NAME)
 $(NAME):
 	wget $(GZURL)
